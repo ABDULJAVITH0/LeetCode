524ed0fee870881065e47274269be9bd4987bd79
ABDULJAVITH0 <javithabdul0@gmail.com>
2026-09-21 13:30:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (95.34%) - LeetSync

---
+++ 4332-check-good-integer/check-good-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int squareSum=0,digitSum=0;
+        while(n>0){
+            int num=n%10;
+            digitSum+=num;
+            squareSum+=num*num;
+            n/=10;
+        }
+        if(squareSum-digitSum>=50){
+            return true;
+        }
+        return false;
+    }
+}
